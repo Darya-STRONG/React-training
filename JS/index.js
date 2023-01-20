6a6992eba1bb4f3a482399684d1aca715f61c0f1
Bakulina Daria <102513287+Darya-STRONG@users.noreply.github.com>
2023-01-20 13:28:23 +0200

    using arrays: some(), every(), find() and operator [...spreads]

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+//Array Cardio Day1
 //some data we can work with
 
 const inventors = [
@@ -83,3 +84,63 @@ const transportation = data.reduce(function(obj, item) {
   obj[item]++;
   return obj;
 },{});
+
+//Array Cardio Day2
+const people = [
+  {name:'Kata', year: 1988},
+  {name:'Dasha', year: 1986},
+  {name:'Daniel', year: 1970},
+  {name:'Alisa', year: 2015},
+];
+
+const comments = [
+  {text:'Love this!', id: 456786},
+  {text:'Super good', id: 456789},
+  {text:'You are the best', id: 823423},
+  {text:'Ramen in my fav food ever', id:756788},
+  {text:'Nice Nice Nice!', id: 567899},
+]
+
+
+//Some and Every Checks
+//Array.prototype.some()// is at least one person 19?
+// const isAdult = people.some(function(person){
+//   const currentYear = (new Data()).getFullYear();
+//   if(currentYear - person.year >= 19) {
+//     return true;
+//   }
+// })
+
+const isAdult = people.some(person => ((new Data()).getFullYear()) -  person.year >= 19);
+
+console.log({isAdult});
+
+//Array.prototype.every()// is everyone 19?
+
+const allAdults = people.every(person => ((new Data()).getFullYear()) -  person.year >= 19);
+console.log(allAdults);
+
+//Array.prototype.find()
+//Find is like filter, but instead returns just the one you are looking for
+//find the comment with the ID of 823423
+// const comment = comments.find (function(comment) {
+//   if(comment.id === 823423) {
+//     return true;
+//   }
+// });
+
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment);
+
+//Array.prototype.findIndex()
+//Find the comment with this ID
+//delete the comment with the ID of 823423
+
+const comment = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+// comments.splice(index, 1);
+
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1),
+];
